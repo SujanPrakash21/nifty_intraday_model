0c516286abb436fc5c4da3f0e76e638d0d52b59c
SujanPrakash21 <sujannprakash67@gmail.com>
2026-08-31 12:44:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,6 @@ import streamlit as st
 import pandas as pd
 import gspread
 from google.oauth2.service_account import Credentials
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 
 # ============================================================
