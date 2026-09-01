8b5926dc52917e63b973964a9f02d5b1003589b0
sujan_21 <sujannprakash67@gmail.com>
2026-09-01 16:06:16 +0530

    made changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -2228,7 +2228,8 @@ with up_column:
             f"**UP Probability | Threshold: {up_threshold:.0%}**\n\n"
             f"## {up_probability_display}\n\n"
             f"**UP Prediction**\n\n"
-            f"## 🟢 UP PREDICTION ⬆️"
+            # f"## 🟢 UP PREDICTION ⬆️"
+            up_prediction_text
         )
 
         st.success(
@@ -2273,7 +2274,8 @@ with down_column:
             f"**DOWN Probability | Threshold: {down_threshold:.0%}**\n\n"
             f"## {down_probability_display}\n\n"
             f"**DOWN Prediction**\n\n"
-            f"## 🔴 DOWN PREDICTION ⬇️"
+            # f"## 🔴 DOWN PREDICTION ⬇️"
+            down_prediction_text
         )
 
         st.error(
