6f96090a4ba2718dcecb98661609e56292bde06d
sujan_21 <sujannprakash67@gmail.com>
2026-09-01 16:06:52 +0530

    added comma

--- streamlit_app.py
+++ streamlit_app.py
@@ -2227,7 +2227,7 @@ with up_column:
             f"🟢 **UP MODEL — ACTIVE**\n\n"
             f"**UP Probability | Threshold: {up_threshold:.0%}**\n\n"
             f"## {up_probability_display}\n\n"
-            f"**UP Prediction**\n\n"
+            f"**UP Prediction**\n\n",
             # f"## 🟢 UP PREDICTION ⬆️"
             up_prediction_text
         )
@@ -2273,7 +2273,7 @@ with down_column:
             f"🔴 **DOWN MODEL — ACTIVE**\n\n"
             f"**DOWN Probability | Threshold: {down_threshold:.0%}**\n\n"
             f"## {down_probability_display}\n\n"
-            f"**DOWN Prediction**\n\n"
+            f"**DOWN Prediction**\n\n",
             # f"## 🔴 DOWN PREDICTION ⬇️"
             down_prediction_text
         )
